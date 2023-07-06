b543e614bc40c8b5279f9009bb9c91371af68d78
Yeabsira Tamiru <ytamiru@terpmail.umd.edu>
2023-07-06 17:36:52 +0000

    modift run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,6 +9,15 @@ def hello_world():
 @app.route("/second_page")
 def second_page():
     return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/DBandAPI/SEO-demo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
